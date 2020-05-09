c929ee29eb67d7684103cd4dc19221c925d3c17f
Kai Yuen Leong <kaiyuenleong@gmail.com>
2020-05-09 19:43:26 -0400

    Add loader component

---
+++ src/components/common/Loader.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { ActivityIndicator, View, StyleSheet } from "react-native";
+
+const Loader = () => {
+  const { loaderStyle } = styles;
+
+  return (
+    <View style={loaderStyle}>
+      <ActivityIndicator />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  loaderStyle: {
+    flex: 1,
+    justifyContent: "center",
+    width: 10,
+    height: 10
+  }
+});
+
+export { Loader };
--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,3 +1,4 @@
 export * from './Gradient';
 export * from './Input';
 export * from './Button';
+export * from './Loader';
