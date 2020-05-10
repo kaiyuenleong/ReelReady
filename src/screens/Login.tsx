45a43c27aa7da88db65d0075f6956dae087b07d2
Kai Yuen Leong <kaiyuenleong@gmail.com>
2020-05-10 01:29:59 -0400

    Refactor login screen styling

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Text, Image, View, TouchableOpacity } from "react-native";
+import { Text, Image, View, TouchableOpacity, TouchableWithoutFeedback, KeyboardAvoidingView, Keyboard, Platform } from "react-native";
 import { LoginScreenRouteProp, LoginScreenNavigationProp } from "../navigator/types";
 import { connect } from "react-redux";
 import { emailChanged, passwordChanged, loginUser } from "../actions";
@@ -20,7 +20,7 @@ interface LoginProps {
 	emailChanged: (text: string) => LoginAction;
 	passwordChanged: (text: string) => LoginAction;
 	loginUser: any;
-	
+
 	route: LoginScreenRouteProp;
 	navigation: LoginScreenNavigationProp;
 }
@@ -58,44 +58,44 @@ class Login extends Component<LoginProps> {
 	render() {
 		return (
 			<Gradient>
-				<View style={styles.contentContainer}>
-					<View style={styles.imageContainer}>
-						<Image
-							source={require('../../assets/images/logo_w_text.png')}
-							resizeMode="contain"
-							style={styles.image}
-						/>
-					</View>
-					<View style={styles.inputContainer}>
-						<Input
-							placeholder="EMAIL"
-							onChangeText={this.onEmailChange}
-							value={this.props.email}
-						/>
-						<Input
-							secureTextEntry
-							placeholder="PASSWORD"
-							onChangeText={this.onPasswordChange}
-							value={this.props.password}
-						/>
-					</View>
-					<View style={styles.buttonContainer}>
-						<View style={styles.textContainer}>
-							<Text style={styles.errorText}>
-								{this.props.error}
-							</Text>
-							<TouchableOpacity onPress={this.forgotPassword}>
-								<Text style={styles.forgotPasswordText}>
-									Forgot password?
-								</Text>
-							</TouchableOpacity>
+				<KeyboardAvoidingView behavior={Platform.OS == "ios" ? "padding" : "height"} style={{ flex: 1 }}>
+					<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+						<View style={styles.contentContainer}>
+							<View style={styles.imageContainer}>
+								<Image
+									source={require('../../assets/images/logo_w_text.png')}
+									resizeMode="contain"
+									style={styles.image}
+								/>
+							</View>
+							<View style={styles.inputContainer}>
+								<Input
+									placeholder="EMAIL"
+									onChangeText={this.onEmailChange}
+									value={this.props.email}
+								/>
+								<Input
+									secureTextEntry
+									placeholder="PASSWORD"
+									onChangeText={this.onPasswordChange}
+									value={this.props.password}
+								/>
+								<View style={styles.textContainer}>
+									<Text style={styles.errorText}>Error Message</Text>
+									<TouchableOpacity onPress={this.forgotPassword}>
+										<Text style={styles.forgotPasswordText}>
+											Forgot password?
+										</Text>
+									</TouchableOpacity>
+								</View>
+								{this.renderButton()}
+								<TouchableOpacity onPress={this.register}>
+									<Text style={styles.registrationText}>New User? Create an Account</Text>
+								</TouchableOpacity>
+							</View>
 						</View>
-						{this.renderButton()}
-						<TouchableOpacity onPress={this.register}>
-							<Text style={styles.registrationText}>New User? Create an Account</Text>
-						</TouchableOpacity>
-					</View>
-				</View>
+					</TouchableWithoutFeedback>
+				</KeyboardAvoidingView>
 			</Gradient>
 		)
 	}
--- src/styles/Login.ts
+++ src/styles/Login.ts
@@ -2,55 +2,52 @@ import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
   contentContainer: {
-    marginLeft: '8%',
-    marginRight: '8%',
+    paddingLeft: 30,
+    paddingRight: 30,
+    paddingTop: 30,
+    paddingBottom: 30,
     flex: 1
   },
   image: { 
-    height: '75%', 
-    width: '75%'
+    height: 250, 
+    width: 250
   },
   registrationText: {
     textAlign: 'center',
     color: '#FFFFFF',
     fontSize: 16,
     fontWeight: '200',
-    marginTop: '5%',
+    marginTop: 20,
     letterSpacing: 0.2
   },
   forgotPasswordText: {
     textAlign: 'right',
     color: '#FFFFFF',
-    fontSize: 13,
+    fontSize: 14,
     fontWeight: '200',
     letterSpacing: 0.2
   },
   errorText: {
-    fontSize: 13,
+    fontSize: 14,
     color: 'red'
   },
   imageContainer: {
     backgroundColor: 'transparent',
-    flex: 3,
-    marginTop: '10%',
+    flex: 1,
     flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center'
   },
   inputContainer: {
     backgroundColor: 'transparent',
-    flex: 1,
+    flex: 1
   },
   textContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between'
-  },
-  buttonContainer: {
-    backgroundColor: 'transparent',
-    flex: 1,
-    flexDirection: 'column',
-    paddingBottom: 20,
-    justifyContent: 'space-between'
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderColor: "red",
+    borderWidth: 1,
+    height: 40
   }
 });
